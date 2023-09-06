f8a5e648441b609f99b36425f56c9f1fe66ca31e
tanishkandira <tanishkandira1049@gmail.com>
2023-09-06 12:03:07 +0530

    Code files for MovieMania

---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2030 <Link to="/" className="hover:underline">MovieMania</Link>. All Rights Reserved.</span>
+          <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+              <li>
+                  <a href="https://www.linkedin.com/in/tanishkandira/" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">LinkedIn</a>
+              </li>
+              <li>
+                  <a href="https://github.com/tanishkandira" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Github</a>
+              </li>
+              <li>
+                <a href="mailto:tanishkandira1049@gmail.com" className="mr-4 hover:underline md:mr-6">Contact Me</a>
+              </li>
+          </ul>
+      </footer>
+  )
+}
